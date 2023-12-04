40f0cfff9579ee163a15617ab999d8bc94f19cb6
Malix-off <alixbrunetcontact@gmail.com>
2023-12-04 22:08:14 +0100

    enhance: rename vscodeGitExtensionApi to vscodeGitExtensionApiInstance

--- src/vscodeGitExtensionApi.ts
+++ src/vscodeGitExtensionApi.ts
@@ -1,4 +1,4 @@
 import { extensions } from 'vscode';
 import { API, GitExtension } from '../lib/vscodeGitExtension';
 
-export const vscodeGitExtensionApi: API = extensions.getExtension<GitExtension>('vscode.git')!.exports.getAPI(1);
+export const vscodeGitExtensionApiInstance: API = extensions.getExtension<GitExtension>('vscode.git')!.exports.getAPI(1);
