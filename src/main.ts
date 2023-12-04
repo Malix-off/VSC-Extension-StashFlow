83176976007c2acf986b8591f3ff341b6d303a3d
Malix-off <alixbrunetcontact@gmail.com>
2023-12-04 16:58:30 +0100

    enhance: main

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { ExtensionContext } from 'vscode';
 import { disposableList } from "./disposables";
 
 export function activate(context: ExtensionContext): void {
-	console.log('StashFlow Enabled');
+	console.log('StashFlow Activated');
 	context.subscriptions.push(...disposableList);
-	console.log('StashFlow Ready');
 }
 
 export function deactivate(): void {
-
+	console.log('StashFlow Deactivated');
 }
