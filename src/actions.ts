68fe08cb9a43f85101deec2111ac408311e5ee4b
Malix-off <alixbrunetcontact@gmail.com>
2023-12-05 21:36:25 +0100

    enhance(fs): merge actions + eventHandlers in vscodeGitExtensionApiClient

--- src/actions.ts
+++
@@ -1,4 +0,0 @@
-// import { extensions } from "vscode";
-// import { GitExtension } from "../lib/git";
-
-// const gitExtension = extensions.getExtension<GitExtension>('vscode.git')!.exports.getAPI(1);
