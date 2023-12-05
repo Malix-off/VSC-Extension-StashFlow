3f8c8e53718f2f521d7b45a548feb2657f9992bf
Malix-off <alixbrunetcontact@gmail.com>
2023-12-05 16:25:36 +0100

    nit: syntax

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { ExtensionContext } from 'vscode';
-import { disposableList } from "./disposables";
+import { disposableList } from './disposables';
 
 export function activate(context: ExtensionContext): void {
 	console.log('StashFlow Activated');
